9f1f1620dfca6e499781a47f97b42724fe982ba3
Benjamin <benjaminklarer@gmail.com>
2024-04-03 15:54:02 -0700

    Initial Setup

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,6 @@
+export { GET, POST } from "@/auth";
+
+// these are how you setup routes in nextjs but auth provides them so we will use those here
+// export async function GET() {}
+
+// export async function POST() {}
